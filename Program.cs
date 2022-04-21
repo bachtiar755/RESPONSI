3dbe8ec0033a9885b7cdb6d038167a727ddd4301
bachtiar755 <101872108+bachtiar755@users.noreply.github.com>
2022-04-21 10:16:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string nama;
+
+            int nip, gpokok, gtunjangan, gtotal;
+
+
+
+            Console.Write("Masukkan Nama  = ");
+
+            nama = (Console.ReadLine());
+
+            Console.Write("Masukkan NIP  = ");
+
+            nip = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            gpokok = 0;
+            gtotal = gtunjangan + gpokok;
+
+
+
+            Console.WriteLine("Nama  : " + nama);
+
+            Console.WriteLine("NIP  : " + nip);
+
+            Console.WriteLine("Gaji Pokok : " + "Rp" + gpokok);
+
+            Console.WriteLine("Tunjangan : " + "Rp" + gtunjangan);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Total gaji  = " + "Rp" + gtotal);
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
